b828f1ccc708f623f724e05ef8c8a0af3fff3477
sailendrachettri <sailendra9083@gmail.com>
2021-09-01 21:22:06 +0530

    sum file added

---
+++ file1/unique_str.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    // string str = {'sai', 'lendra'};
+    // cout<< "Unique: "<<unique(str.begin(), str.end())<<endl;
+    float a = 10/4;
+    cout<< a<<endl;
+
+    return 0;
+}
