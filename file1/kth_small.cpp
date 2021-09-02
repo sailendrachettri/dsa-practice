5c788ecbece6b4bfe3153cf6e53e87cfa85490c5
sailendrachettri <sailendra9083@gmail.com>
2021-09-02 08:18:13 +0530

    sorting an array

---
+++ file1/kth_small.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    // int arr[] = {7, 10, 4, 3, 20, 15};
+    int arr[] = { 7, 10, 4, 20, 15};
+    int k = 4;
+    int size = sizeof(arr[0]);
+    cout<< "size: "<<size<<endl;
+/*
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << "" << endl;
+
+    sort(arr, arr + size);
+
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << "" << endl;
+
+    cout << "Thied smallers element is "<< arr[k-1] << endl;
+    */
+    return 0;
+}
--- file1/min_max_brootforce.cpp
+++ file1/min_max_brootforce.cpp
@@ -4,22 +4,25 @@ using namespace std;
 
 int main()
 {
-    vector <int> arr = {2, 23, 9, 3, 1, 4, 12};
+    int arr[] = {2, 23, 9, 3, 1, 4, 12};
     int min = arr[0], max = 0, size = sizeof(arr)/ sizeof(arr[0]);
 
-    min = *min_element(arr.begin(), arr.end());
-    max = *max_element(arr.begin(), arr.end());
-
-    // for (int i = 0; i < size; i++)
-    // {
-    //     if(arr[i] >= max){
-    //         max = arr[i];
-    //     }
+    for (int i = 0; i < size; i++)
+    {
+        if(arr[i] >= max){
+            max = arr[i];
+        }
         
-    //     if(arr[i] <= min){
-    //         min = arr[i];
-    //     }   
-    // }
+        if(arr[i] <= min){
+            min = arr[i];
+        }   
+    }
+
+    // second method
+    // int min = arr[0], max = 0, size = sizeof(arr)/ sizeof(arr[0]);
+    // vector <int> arr = {2, 23, 9, 3, 1, 4, 12};
+    // min = *min_element(arr.begin(), arr.end());
+    // max = *max_element(arr.begin(), arr.end());
 
     cout<< "Max = "<< max<<endl;
     cout<< "Min = "<< min<<endl;
