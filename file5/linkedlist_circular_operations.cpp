4f803e90f21d318e916936651f9eb171b3519cf2
sailendrachettri <sailendra9083@gmail.com>
2021-09-11 05:47:14 +0530

    circular linkedlist traversal

---
+++ file5/linkedlist_circular_operations.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+struct Node
+{
+    int data;
+    struct Node *next;
+};
+
+void linkedListTraversal(struct Node *head)
+{
+    struct Node *ptr = head;
+
+    do
+    {
+        cout << "Element is " << ptr->data << endl;
+        ptr = ptr->next;
+    } while (ptr != head); // only while loop doesn't work becaues this condition become false at the beginning
+}
+
+int main()
+{
+    struct Node *head = (struct Node *)malloc(sizeof(struct Node));
+    struct Node *second = (struct Node *)malloc(sizeof(struct Node));
+    struct Node *third = (struct Node *)malloc(sizeof(struct Node));
+    struct Node *fourth = (struct Node *)malloc(sizeof(struct Node));
+
+    head->data = 4;
+    head->next = second;
+
+    second->data = 3;
+    second->next = third;
+
+    third->data = 6;
+    third->next = fourth;
+
+    fourth->data = 1;
+    fourth->next = head;
+
+    linkedListTraversal(head);
+    return 0;
+}
