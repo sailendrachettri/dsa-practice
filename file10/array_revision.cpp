a5b62050afdf026cf2d269efaa7ae52f3625c0cb
sailendrachettri <sailendra9083@gmail.com>
2021-09-27 06:35:50 +0530

    deletion operation in array

--- file10/array_revision.cpp
+++ file10/array_revision.cpp
@@ -18,6 +18,13 @@ void insertion(int arr[], int size, int index, int element)
     arr[index] = element;
 }
 
+void deletion(int arr[], int index, int size){
+    for (int i = index; i < size; i++)
+    {
+        arr[i] = arr[i + 1];
+    }
+}
+
 int main()
 {
     int arr[] = {10, 20, 30, 40, 50};
@@ -25,12 +32,19 @@ int main()
 
     traversal(arr, size);
 
-    int index = 3;
-    int element = 100;
-    insertion(arr, size, index, element);
-    size++;
+    // Insertion operation
+    // int index = 5;
+    // int element = 100;
+    // insertion(arr, size, index, element);
+    // size++;
+    // cout<< "After insertion: "<<endl;
 
-    cout<< "After insertion: "<<endl;
+    // Deletion operation
+    int index = 3;
+    deletion(arr, index, size);
+    cout<< "After Deletion"<<endl;
+    size--;
+
     traversal(arr, size);
     return 0;
 }
