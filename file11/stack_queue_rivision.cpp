40bd6ff2e214a5a89ac8b9d75299dab77957ff0b
sailendrachettri <sailendra9083@gmail.com>
2021-09-30 05:41:03 +0530

    push operation on stack - revision

--- file11/stack_queue_rivision.cpp
+++ file11/stack_queue_rivision.cpp
@@ -21,7 +21,7 @@ int isFull(struct stack *ptr){
 void push(struct stack *ptr, int val){
     if(isFull(ptr)) cout<< "Stack OverFlow"<<endl;
     else{
-        ptr->size++;
+        ptr->top++;
         ptr->arr[ptr->top] = val;
         cout<< val <<" is pushed."<<endl;
     }
@@ -30,11 +30,19 @@ void push(struct stack *ptr, int val){
 int main()
 {
     struct stack *s = (struct stack *) malloc(sizeof(struct stack));
-    s->size = 80;
+    s->size = 3;
     s->top = -1;
     s->arr = (int *) malloc(s->size * sizeof(int));
 
     cout<<isEmpty(s)<<endl;
     cout<<isFull(s)<<endl;
+
+    push(s, 10);
+    push(s, 20);
+    push(s, 30);
+    // push(s, 40); // can't push this element coz it causes stack over flow
+    cout<<isEmpty(s)<<endl;
+    cout<<isFull(s)<<endl;
+
     return 0;
 }
